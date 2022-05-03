a2e333389f96d1d5d00ff12cfe9ad47c56452420
Clarence Cheung <clarence.e.cheung@gmail.com>
2022-05-03 23:02:24 +1000

    just testing stuff

--- app.py
+++ app.py
@@ -1,11 +1,53 @@
-from flask import Markup, Flask, render_template
+from operator import truediv
+from re import A
+from flask import Markup, Flask, render_template, Response, url_for
+import cv2
 from chess_stuff import stockfish_test
 import chess.svg
-
+import chess
+import datetime
+import time
+a = 0
+b = stockfish_test.board
 app = Flask(__name__)
-
 @app.route('/')
 def main():
-    svg = chess.svg.board(stockfish_test.board)
-    # return render_template("test.html", svg=Markup(svg))
-    return Markup(svg)
+    # svg = chess.svg.board(stockfish_test.board)
+    return render_template("test.html")
+
+@app.route('/video_feed')
+def video_feed():
+		# Set to global because we refer the video variable on global scope, 
+		# Or in other words outside the function
+    global b
+		# Return the result on the web
+  
+    return Response(abc(b),
+                    mimetype='image/svg+xml; multipart/x-mixed-replace; boundary=frame')
+    # return Response(chess.svg.board(b, size=400), mimetype='image/svg+xml')
+
+@app.route('/update')
+def update():
+  b.push(stockfish_test.engine.play(b, chess.engine.Limit(time=1)).move)
+  return {"status": "success"}
+
+def abc(b):
+  # while True:
+    # board.push(stockfish_test.engine.play(board, chess.engine.Limit(time=1)).move)
+  svg = chess.svg.board(b, size=400)
+  # print(board)
+  # return (svg)
+  yield svg
+
+def gay(a):
+  while True:
+    a = a + 1
+    return str(a) 
+
+@app.route('/lmao')
+def lmao():
+    global a
+    a = a + 1
+    # return Response(gay(a), mimetype="text")
+    return {"now" : gay(a)}
+app.run()
--- chess_stuff/stockfish_test.py
+++ chess_stuff/stockfish_test.py
@@ -1,6 +1,3 @@
-# from stockfish import Stockfish
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtSvg import QSvgWidget, QSvgRenderer
 import chess
 import chess.engine
 import chess.svg
@@ -9,12 +6,6 @@ import sys
 # stockfish = Stockfish(path="./stockfish testing/stockfish_15_x64_avx2")
 board = chess.Board()
 engine = chess.engine.SimpleEngine.popen_uci("./stockfish_15_x64_avx2")
-
-# app = QApplication(sys.argv)
-# svgWidget = QSvgWidget()
-# svgWidget.load(chess.svg.board(board).encode("UTF-8"))
-# svgWidget.setGeometry(100,100,300,300)
-# svgWidget.show()
 
 # info = engine.play(board, chess.engine.Limit(time=1))
 # board.push(info.move)
