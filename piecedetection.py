6a8d3924bf0172ffc54cd751d7f1e572639d57ab
leonwangg1 <leonwang148@gmail.com>
2022-05-27 21:36:13 +1000

    asd

--- piecedetection.py
+++ piecedetection.py
@@ -15,6 +15,7 @@ import base64
 import numpy as np
 import requests
 import time
+from PIL import Image
 
 # Construct the Roboflow Infer URL
 # (if running locally replace https://detect.roboflow.com/ with eg http://127.0.0.1:9001/)
@@ -27,33 +28,37 @@ upload_url = "".join([
     "&stroke=5"
 ])
 
-# Get webcam interface via opencv-python
 video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 # Infer via the Roboflow Infer API and return the result
 def infer():
-    # Get the current image from the webcam
-    ret, img = video.read()
+    # check camera is open
+    if video.isOpened():
+        # Get the current image from the webcam
+        ret, img = video.read()
+        if img is not None:
+            # Resize (while maintaining the aspect ratio) to improve speed and save bandwidth
+            height, width, channels = img.shape
+            scale = ROBOFLOW_SIZE / max(height, width)
+            img = cv2.resize(img, (round(scale * width), round(scale * height)))
+            
+            # Encode image to base64 string
+            retval, buffer = cv2.imencode('.jpg', img)
+            img_str = base64.b64encode(buffer)
 
-    # Resize (while maintaining the aspect ratio) to improve speed and save bandwidth
-    height, width, channels = img.shape
-    scale = ROBOFLOW_SIZE / max(height, width)
-    img = cv2.resize(img, (round(scale * width), round(scale * height)))
+            # Get prediction from Roboflow Infer API
+            resp = requests.post(upload_url, data=img_str, headers={
+                "Content-Type": "application/x-www-form-urlencoded"
+            }, stream=True).raw
 
-    # Encode image to base64 string
-    retval, buffer = cv2.imencode('.jpg', img)
-    img_str = base64.b64encode(buffer)
-
-    # Get prediction from Roboflow Infer API
-    resp = requests.post(upload_url, data=img_str, headers={
-        "Content-Type": "application/x-www-form-urlencoded"
-    }, stream=True).raw
-
-    # Parse result image
-    image = np.asarray(bytearray(resp.read()), dtype="uint8")
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-
-    return image
+            # Parse result image
+            image = np.asarray(bytearray(resp.read()), dtype="uint8")
+            image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+            cv2.imshow('image', image)
+            cv2.waitKey(1)
+            return image
+    else:
+        ret = False
 
 # Main loop; infers sequentially until you press "q"
 while 1:
@@ -61,17 +66,13 @@ while 1:
     if(cv2.waitKey(1) == ord('q')):
         break
 
-    # Capture start time to calculate fps
-    start = time.time()
-
     # Synchronously get a prediction from the Roboflow Infer API
     image = infer()
     # And display the inference results
-    cv2.imshow('image', image)
+    # if image is not None:
+    #     print('showing frame')
+    #     cv2.imshow('image', image)
+    #     cv2.waitKey(1)
+    # else:
+    #     print('fail')
 
-    # Print frames per second
-    print((1/(time.time()-start)), " fps")
-
-# Release resources when finished
-video.release()
-cv2.destroyAllWindows()
