69a8e4ae8fd97fa79e81b48a9982582a22e06595
leonwangg1 <leonwang148@gmail.com>
2022-05-28 13:28:01 +1000

    detectron2

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
 from flask import Markup, Flask, render_template, Response, url_for
-import cv2
-import chess.svg
-import chess.engine
-import chess
-import numpy as np
-import requests
-# import piecedetection
+import threading
+import chesspiecedetection
 
 app = Flask(__name__)
 
-# model = torch.hub.load('yolov5-master', 'custom', path='best.pt', source='local')
+lock = threading.Lock()
 
 @app.route('/')
 def main():
     return render_template("test.html")
 
-# @app.route('/stream',methods = ['GET'])
-# def stream():
-#    return Response(generate(), mimetype = "multipart/x-mixed-replace; boundary=frame")
-
-# def generate():
-#     while 1:
-#         image = piecedetection.infer()
-#         return image
-        
 app.run()
+
+@app.route('/stream',methods = ['GET'])
+def stream():
+   return Response(chesspiecedetection.main(), mimetype = "multipart/x-mixed-replace; boundary=frame")
---
+++ chesspiecedetection.py
@@ -0,0 +1,70 @@
+# install dependencies: (use cu101 because colab has CUDA 10.1)
+# pip install -U torch==1.5 torchvision==0.6 -f https://download.pytorch.org/whl/cu101/torch_stable.html 
+# pip install cython pyyaml==5.1
+# git clone https://github.com/facebookresearch/detectron2.git
+# pip install -e detectron2
+
+import cv2
+import torch
+print(torch.__version__, torch.cuda.is_available())
+import os
+import time
+from scipy import ndimage
+import detectron2
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+from detectron2.data.catalog import DatasetCatalog
+from detectron2.utils.visualizer import ColorMode
+from detectron2.data.datasets import register_coco_instances
+
+# Register dataset for detectron2
+def load_dataset():
+    register_coco_instances("my_dataset_train", {}, "dataset/train/_annotations.coco.json", "dataset/train")
+    register_coco_instances("my_dataset_val", {}, "dataset/valid/_annotations.coco.json", "dataset/valid")
+    my_dataset_train_metadata = MetadataCatalog.get("my_dataset_train")
+    dataset_dicts = DatasetCatalog.get("my_dataset_train")
+
+# Load model
+def load_model():
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file('COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml'))
+    cfg.DATASETS.TRAIN = ("my_dataset_train",)
+    cfg.DATASETS.TEST = ("my_dataset_val",)
+    cfg.MODEL.WEIGHTS = 'models\model_final.pth' # Set path model .pth
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 14
+    test_metadata = MetadataCatalog.get("my_dataset_test")
+    key = "instances"
+    return cfg
+
+def main():
+    global encodedImage
+    register_coco_instances("my_dataset_train", {}, "dataset/train/_annotations.coco.json", "dataset/train")
+    register_coco_instances("my_dataset_val", {}, "dataset/valid/_annotations.coco.json", "dataset/valid")
+    my_dataset_train_metadata = MetadataCatalog.get("my_dataset_train")
+    dataset_dicts = DatasetCatalog.get("my_dataset_train")
+
+    cfg = load_model()
+    test_metadata = MetadataCatalog.get("my_dataset_test")
+    key = "instances"
+
+    predictor = DefaultPredictor(cfg)
+    vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    while True:
+        # check camera is open
+        if vid.isOpened():
+                ret, frame = vid.read()
+                outputs = predictor(frame)
+                v = Visualizer(frame[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=0.5, instance_mode=ColorMode.SEGMENTATION)
+                out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+                (flag, encodedImage) = cv2.imencode(".jpg", out.get_image()[:, :, ::-1])
+                yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+                # cv2.imshow("frame", out.get_image()[:, :, ::-1])
+                # cv2.waitKey(1)
+        else:
+            ret = False
+
